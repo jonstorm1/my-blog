615bc4291aa113d7fbeb6699b78703327535237e
Jon <jonstorm1@icloud.com>
2023-02-11 11:43:01 -0500

    erqwer

--- frontend/pages/post/[slug].tsx
+++ frontend/pages/post/[slug].tsx
@@ -1,14 +1,39 @@
-// post.tsx
-import { useRouter } from 'next/router'
+// ./frontend/pages/post/[slug].tsx
 
-const Post = () => {
-  const router = useRouter()
+import client from '../../client'
+
+const Post = ({post}) => {
   
   return (
     <article>
-      <h1>{router.query.slug}</h1>
+      <h1>{post?.slug?.current}</h1>
     </article>
   )
+}
+
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(`
+    *[_type == "post" && slug.current == $slug][0]
+  `, { slug })
+  
+  return {
+    props: {
+      post
+    }
+  }
 }
 
 export default Post
