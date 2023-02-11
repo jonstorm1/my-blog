fd06ad2e73b67a886e849fccd845b6f884ab4bd6
Jon <jonstorm1@icloud.com>
2023-02-11 18:15:08 -0500

    rerewq

--- frontend/pages/post/[slug].tsx
+++ frontend/pages/post/[slug].tsx
@@ -2,10 +2,28 @@
 
 import groq from 'groq'
 import imageUrlBuilder from '@sanity/image-url'
+import {PortableText} from '@portabletext/react'
 import client from '../../client'
 
 function urlFor (source) {
   return imageUrlBuilder(client).image(source)
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value }) => {
+      if (!value?.asset?._ref) {
+        return null
+      }
+      return (
+        <img
+          alt={value.alt || ' '}
+          loading="lazy"
+          src={urlFor(value).width(320).height(240).fit('max').auto('format')}
+        />
+      )
+    }
+  }
 }
 
 const Post = ({post}) => {
@@ -13,7 +31,8 @@ const Post = ({post}) => {
     title = 'Missing title',
     name = 'Missing name',
     categories,
-    authorImage
+    authorImage,
+    body = []
   } = post
   return (
     <article>
@@ -31,9 +50,14 @@ const Post = ({post}) => {
             src={urlFor(authorImage)
               .width(50)
               .url()}
+            alt={`${name}'s picture`}
           />
         </div>
       )}
+      <PortableText
+        value={body}
+        components={ptComponents}
+      />
     </article>
   )
 }
@@ -42,9 +66,9 @@ const query = groq`*[_type == "post" && slug.current == $slug][0]{
   title,
   "name": author->name,
   "categories": categories[]->title,
-  "authorImage": author->image
+  "authorImage": author->image,
+  body
 }`
-
 export async function getStaticPaths() {
   const paths = await client.fetch(
     groq`*[_type == "post" && defined(slug.current)][].slug.current`
